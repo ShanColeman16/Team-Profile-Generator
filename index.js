1b9aef447fbb2841c6c4f6406b95c3c0b4bf2604
unknown <shannoncoleman16@gmail.com>
2023-01-03 23:58:07 -0500

    added questions

--- index.js
+++ index.js
@@ -1,3 +1,69 @@
 const Manager = require('./lib/Manager');
+const questions = [
+    {
+        type: "input",
+        name: "title",
+        message: "Please enter your manager's name?"
+    },
+    {
+        type: "input",
+        name: "github",
+        message: "What is your employee id?"
+    },
+    {
+        type: "input",
+        name: "email",
+        message: "What is your email address?"
+    },
+    {
+        type: "input",
+        name: "email",
+        message: "What is your office number?"
+    },
+];
 const Engineer = require('./lib/Engineer');
+const Engineerquestions = [
+    {
+        type: "input",
+        name: "title",
+        message: "Engineer? Please enter your name?"
+    },
+    {
+        type: "input",
+        name: "github",
+        message: "What is your employee id?"
+    },
+    {
+        type: "input",
+        name: "email",
+        message: "What is your email address?"
+    },
+    {
+        type: "input",
+        name: "email",
+        message: "What is GitHub username?"
+    },
+];
 const Intern = require('./lib/Intern');
+const Internquestions = [
+    {
+        type: "input",
+        name: "title",
+        message: "Intern? Please enter your name?"
+    },
+    {
+        type: "input",
+        name: "github",
+        message: "What is your employee id?"
+    },
+    {
+        type: "input",
+        name: "email",
+        message: "What is the name of your school?"
+    },
+    {
+        type: "input",
+        name: "email",
+        message: "What is your office number?"
+    },
+];
