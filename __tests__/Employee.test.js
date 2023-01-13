e143798b508cf2b796e5e9b32ab3efbb49e1029d
unknown <shannoncoleman16@gmail.com>
2023-01-12 21:22:10 -0500

    added tests to employee

--- __tests__/Employee.test.js
+++ __tests__/Employee.test.js
@@ -1,8 +1,11 @@
 const Employee = require("../lib/Employee");
 
 describe("Employee", () => {
-  it ('has an id, email, and name', () => {
-    const employee = new Employee('Derrion', 1, 'coolbreeze0616@gmail.com');
-
+  it ('has an name', () => {
+    // const employee = new Employee('Derrion', 1, 'coolbreeze0616@gmail.com');
+    // expect(employee).toBe(true);
   });
+  it ('has an id', () => {})
+  it ('has a email', () => {})
+  it ('has a role', () => {})
 });
