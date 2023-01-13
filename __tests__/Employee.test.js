a475a8641f32a60260b0226038b1887dcd4aed2e
unknown <shannoncoleman16@gmail.com>
2023-01-12 23:00:57 -0500

    added function to engineer

--- __tests__/Employee.test.js
+++ __tests__/Employee.test.js
@@ -9,6 +9,12 @@ describe("Employee", () => {
     const employee = new Employee('Derrion', 1, 'coolbreeze0616@gmail.com');
     expect(employee.id).toBe('1');
   })
-  it ('has a email', () => {})
-  it ('has a role', () => {})
+  it ('has a email', () => {
+    const employee = new Employee('Derrion', 1, 'coolbreeze0616@gmail.com');
+    expect(employee.email).toBe('coolbreeze0616@gmail.com');
+  })
+  it ('has a role', () => {
+    const employee = new Employee('Derrion', 1, 'coolbreeze0616@gmail.com');
+    expect(employee.getRole).toBe('Employee');
+  })
 });
--- __tests__/Engineer.test.js
+++ __tests__/Engineer.test.js
@@ -1,5 +1,8 @@
 const Engineer = require("../lib/Engineer");
 
 describe("Engineer", () => {
-  it ('has a GitHub', () => {})
+  it ('has a GitHub', () => {
+    const engineer = new Engineer('Derrion', 1, 'coolbreeze0616@gmail.com.', 'ShanColeman16');
+    expect(engineer.gitHub).toBe('ShanColeman16');
+  });
 });
