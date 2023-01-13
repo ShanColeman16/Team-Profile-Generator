6a54cfc932fd3c4b649311ef0dd90398780c27e1
unknown <shannoncoleman16@gmail.com>
2023-01-12 23:04:45 -0500

    added function to intern

--- __tests__/Intern.test.js
+++ __tests__/Intern.test.js
@@ -1,5 +1,8 @@
 const Intern = require("../lib/Intern");
 
 describe("Intern", () => {
-  it ('has a school', () => {})
+  it ('has a school', () => {
+    const intern = new Intern('Derrion', 1, 'coolbreeze0616@gmail.com.', 'ShanColeman16');
+    expect(intern.getGitHub).toBe('UNCC');
+  });
 });
