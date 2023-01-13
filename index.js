d8ca8fd47bf15e62521297064e848edd499d2962
unknown <shannoncoleman16@gmail.com>
2023-01-13 16:01:59 -0500

    added functions

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ const inquirer = require("inquirer");
 const Engineer = require("../lib/Engineer");
 const Intern = require("../lib/Intern");
 const Manager = require('./lib/Manager');
+const fs = require('fs');
+
 
 const questions = [
     {
