4cf012c16559cd9cb871547e5acb4dc5ffb13af2
unknown <shannoncoleman16@gmail.com>
2023-01-12 21:12:27 -0500

    added it statment to employee

--- __tests__/Employee.test.js
+++ __tests__/Employee.test.js
@@ -1,5 +1,8 @@
 const Employee = require("../lib/Employee");
 
 describe("Employee", () => {
+  it ('has an id, email, and name', () => {
+    const employee = new Employee('Derrion', 1, 'coolbreeze0616@gmail.com');
 
+  });
 });
--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const inquirer = require("inquirer");
-
+const Engineer = require("../lib/Engineer");
+const Intern = require("../lib/Intern");
 const Manager = require('./lib/Manager');
+
 const questions = [
     {
         type: "input",
