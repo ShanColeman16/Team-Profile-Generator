b2dd75bd0d935de5260882ae9dc7df5cdea2e62d
unknown <shannoncoleman16@gmail.com>
2023-01-12 21:28:58 -0500

    setup function

--- __tests__/Employee.test.js
+++ __tests__/Employee.test.js
@@ -2,10 +2,13 @@ const Employee = require("../lib/Employee");
 
 describe("Employee", () => {
   it ('has an name', () => {
-    // const employee = new Employee('Derrion', 1, 'coolbreeze0616@gmail.com');
-    // expect(employee).toBe(true);
+    const employee = new Employee('Derrion', 1, 'coolbreeze0616@gmail.com');
+    expect(employee.name).toBe('Derrion');
   });
-  it ('has an id', () => {})
+  it ('has an id', () => {
+    const employee = new Employee('Derrion', 1, 'coolbreeze0616@gmail.com');
+    expect(employee.id).toBe('1');
+  })
   it ('has a email', () => {})
   it ('has a role', () => {})
 });
