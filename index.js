88ac9752b409ea8f1db206ffcbef5dc8058ecb36
unknown <shannoncoleman16@gmail.com>
2023-01-14 00:06:33 -0500

    added prompts

--- index.js
+++ index.js
@@ -1,75 +1,74 @@
-const inquirer = require("inquirer");
-const Engineer = require("../lib/Engineer");
-const Intern = require("../lib/Intern");
+const inquirer = require('inquirer');
+const Engineer = require('./lib/Engineer');
+const Intern = require('./lib/Intern');
 const Manager = require('./lib/Manager');
 const fs = require('fs');
 const prompt = inquirer.createPromptModule();
 
-const questions = [
+const yourTeams = [];
+prompt = ([
     {
-        type: "input",
-        name: 'name',
-        message: 'Please enter your manager/s name?'
+      message: 'Please enter your manager/s name?',
+      name: 'name',
     },
     {
-        type: "input",
-        name: 'id',
-        message: 'What is your manager/s id?'
+      message: 'What is your manager/s id?',
+      name: 'id',
     },
     {
-        type: "input",
-        name: 'email',
-        message: 'What is your manager/s email address?'
+      message: 'What is your manager/s email address?',
+      name: 'email',
     },
     {
-        type: "input",
-        name: 'office number',
-        message: 'What is your manager/s office number?'
+      message: 'What is your manager/s office number?',
+      name: 'office number',
     },
-];
-const Engineer = require('./lib/Engineer');
-const questions2 = [
-    {
-        type: "input",
-        name: "title",
-        message: "Engineer? Please enter your name?"
-    },
-    {
-        type: "input",
-        name: "github",
-        message: "What is your employee id?"
-    },
-    {
-        type: "input",
-        name: "email",
-        message: "What is your email address?"
-    },
-    {
-        type: "input",
-        name: "email",
-        message: "What is GitHub username?"
-    },
-];
-const Intern = require('./lib/Intern');
-const questions3 = [
-    {
-        type: "input",
-        name: "title",
-        message: "Intern? Please enter your name?"
-    },
-    {
-        type: "input",
-        name: "github",
-        message: "What is your employee id?"
-    },
-    {
-        type: "input",
-        name: "email",
-        message: "What is the name of your school?"
-    },
-    {
-        type: "input",
-        name: "email",
-        message: "What is your office number?"
-    },
-];
+
+]) .then((manager) => {
+    console.log(manager);
+});
+
+// const questions2 = [
+//     {
+//         type: "input",
+//         name: "title",
+//         message: "Engineer? Please enter your name?"
+//     },
+//     {
+//         type: "input",
+//         name: "github",
+//         message: "What is your employee id?"
+//     },
+//     {
+//         type: "input",
+//         name: "email",
+//         message: "What is your email address?"
+//     },
+//     {
+//         type: "input",
+//         name: "email",
+//         message: "What is GitHub username?"
+//     },
+// ];
+// const questions3 = [
+//     {
+//         type: "input",
+//         name: "title",
+//         message: "Intern? Please enter your name?"
+//     },
+//     {
+//         type: "input",
+//         name: "github",
+//         message: "What is your employee id?"
+//     },
+//     {
+//         type: "input",
+//         name: "email",
+//         message: "What is the name of your school?"
+//     },
+//     {
+//         type: "input",
+//         name: "email",
+//         message: "What is your office number?"
+//     },
+// ];
